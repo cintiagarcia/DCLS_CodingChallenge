e6822dc877c8ab71986e0d46d42fc06d1f74d1b8
Cintia <cintia@cintias-mbp.lan>
2021-10-11 14:08:51 +0200

    new changes

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-//require("dotenv").config();
+require("dotenv/config");
 require("./db");
 const express = require('express');
-const mongoose = require("mongoose");
+
+const mongoose = require('mongoose');
 
 const app = express();
 
@@ -9,16 +10,17 @@ require("./config")(app);
 
 const session = require("express-session");
 const MongoStore = require("connect-mongo");
-const DB_URL= process.env.MONGODB_URI || "mongodb://localhost/DCLS_CHALLENGE";
+const DB_URL = process.env.MONGODB_URI 
+
 
 app.use(
     session({
-        secret: "coding",
+        secret: process.env.SESSION_SECRET,
         cookie: {maxAge: 1000 * 60 * 60 * 24},
         saveUninitialized: false,
         resave: true,
         store: MongoStore.create({
-            mongoUrl: DB_URL,
+            mongoUrl: process.env.MONGODB_URI,
         }),
 
     })
